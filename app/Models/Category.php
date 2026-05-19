b598049643d5aff441cf46291ed2e6007aaa1a36
Ice Stream Dev <dev@local>
2026-05-19 12:27:38 +0700

    Allow mass assignment for Category: add  (name, description)

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Video;
 
 class Category extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function videos(): HasMany
+    {
+        return $this->hasMany(Video::class);
+    }
 }
